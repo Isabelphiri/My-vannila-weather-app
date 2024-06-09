2b9504d5e906db354bda2bcd0e1c4a8c896cf349
Isabelphiri <isabelphiridon@gmail.com>
2024-06-09 21:23:20 +0200

    made changes in my css and java script

--- src/index.js
+++ src/index.js
@@ -51,49 +51,54 @@ function searchCity(city) {
 function handleSearchSubmit (event) {
     event.preventDefault();
 let searchInput =document.querySelector("#search-form-input");
-
 searchCity(searchInput.value);
 }
+function formatDay(timestamp){
+    let date= new Date(timestamp * 1000);
+    let days=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+    return days[date.getDay()];
+}
+
+
+
+
 function getForecast (city){
     let apikey ="d7tdaf04od04a52e37aa3fb734223b3c";
     let apiUrl =`https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apikey}&units=metric`;
-    axios(apiUrl).then(displayForecast);
+    axios.get(apiUrl).then(displayForecast);
 
 }
 function displayForecast(response){
-    console.log(response.data);
-}
+    
+    
 
 
 
-
-
-function displayForecast(){
-    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
     let forecastHtml ="";
-
-    days.forEach(function (day) {
+    response.data.daily.forEach(function (day, index) {
+        if (index < 5) {
         forecastHtml =
         forecastHtml +`
         <div class="weather-forecast-day">
-        <div class"weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">⛈️</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
         <div class="weather-forecast-temperatures">
         <div class="weather-forcast-temperature">
-        <strong> 15° </strong>
+        <strong>${Math.round(day.temperature.maximum)}° </strong>
         </div>
-        <div class="weather-forecast-temperature"> 9° </div>
+        <div class="weather-forecast-min-temperature"> ${Math.round(day.temperature.minimum)}° </div>
         </div>
         </div>`;
+        }
     });
     let forecastElement = document.querySelector("#forecast");
     forecastElement.innerHTML = forecastHtml;
-    }
+}
+    
 
 
  let searchFormElement = document.querySelector("#search-form");
  searchFormElement .addEventListener("submit",handleSearchSubmit);
 
  searchCity("Paris");
- displayForecast();
-  
+ 
